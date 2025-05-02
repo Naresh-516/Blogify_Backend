89be18cbfbec0925c80e6016abe828f9b776bda4
Naresh-516 <nareshkashetti1@gmail.com>
2025-05-02 20:04:36 +0530

    frontend Link updated

--- src/main/java/com/nt/controller/BlogOperationController.java
+++ src/main/java/com/nt/controller/BlogOperationController.java
@@ -19,7 +19,7 @@ import com.nt.dto.BlogResponseDTO;
 import com.nt.service.BlogServiceImpl;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173") 
+@CrossOrigin(origins = "https://blogifyverse.netlify.app") 
 @RequestMapping("/blog")
 public class BlogOperationController {
 	@Autowired
